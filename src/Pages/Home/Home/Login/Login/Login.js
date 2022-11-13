84b10ff867f14dc41323f16371b1c402c9e9fde3
robin mahmud <robinmahmud358@gmail.com>
2022-11-13 21:35:20 +0600

    password added feature

--- src/Pages/Home/Home/Login/Login/Login.js
+++ src/Pages/Home/Home/Login/Login/Login.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-    const { register, handleSubmit } = useForm()
+    const { register, formState: { errors }, handleSubmit } = useForm()
     const handleLogin = data => {
         console.log(data)
     }
@@ -17,14 +17,21 @@ const Login = () => {
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" {...register("email")} className="input input-bordered w-full max-w-xs" />
+                        <input type="email" {...register("email", {
+                            required: "Email Address is required"
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" {...register("password")} className="input input-bordered w-full max-w-xs" />
+                        <input type="password" {...register("password", {
+                            required: "Email Password is required",
+                            minLength: { value: 6, message: 'Password Must be 6 character Longer' }
+                        })} className="input input-bordered w-full max-w-xs" />
                         <span className="label-text">Forget Password?</span>
+                        {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
                     </div>
                     <input className='btn btn-accent w-full' value='Login' type="submit" />
                     <p>New To Portal <Link className='text-secondary' to='/signup'>Create New Account</Link></p>
